32641af0586074e0e0f098c8eedefc8a8371932c
Gcbicca <biccag@gmail.com>
2023-01-11 18:43:17 -0300

    Criado atributos complexos

--- oo/pessoas.py
+++ oo/pessoas.py
@@ -1,16 +1,21 @@
 class Pessoas:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoas('Luciano')
-    print(Pessoas.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Gabriel'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoas(nome='Renzo')
+    gabriel = Pessoas(nome='gabriel')
+    luciano = Pessoas(renzo, gabriel, nome='Luciano')
+    print(Pessoas.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
