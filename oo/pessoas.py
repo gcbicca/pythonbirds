9121ffc64b67505cbaffc7296cd060c906173951
Gcbicca <biccag@gmail.com>
2023-01-11 18:13:17 -0300

    Criado metodo cumprimentar

--- oo/pessoas.py
+++ oo/pessoas.py
@@ -1,2 +1,9 @@
 class Pessoas:
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoas()
+    print(Pessoas.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
