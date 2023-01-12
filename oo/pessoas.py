a4baaa90f259b4e3e68a092a14450dbeb5ebdb23
Gcbicca <biccag@gmail.com>
2023-01-12 09:31:58 -0300

    Criado e removido atributo dinamico do objets do tipo Pessoa

--- oo/pessoas.py
+++ oo/pessoas.py
@@ -1,15 +1,15 @@
 class Pessoas:
     def __init__(self, *filhos, nome=None, idade=35):
-        self.idade = idade
+        self.idade = idade  # ATRIBUTOS DE INSTÂNCIAS
         self.nome = nome
-        self.filhos = list(filhos)
+        self.filhos = list(filhos)  # ATRIBUTOS COMPLEXOS
 
-    def cumprimentar(self):
+    def cumprimentar(self):  # MÉTODO
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    renzo = Pessoas(nome='Renzo')
+    renzo = Pessoas(nome='Renzo')  # OBJETO
     gabriel = Pessoas(nome='gabriel')
     luciano = Pessoas(renzo, gabriel, nome='Luciano')
     print(Pessoas.cumprimentar(luciano))
@@ -19,3 +19,7 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Ramalho'  # Isso é um ATRIBUTO DINAMICO, especifico ao OBJETO
+    del luciano.filhos
+    print(luciano.__dict__) # Se consegue ver os atributos(ATRIBUTOS DE INSTANCIA) criado para cada OBJETO/
+    print(renzo.__dict__)
